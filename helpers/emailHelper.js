ab3c53159bba58cae609c43417454b70c5a36a9c
sac <sj7717620@gmail.com>
2025-10-13 22:35:53 +0530

    Use MongoDB Atlas for deployment

--- helpers/emailHelper.js
+++ helpers/emailHelper.js
@@ -1,55 +1,30 @@
-const nodemailer = require("nodemailer");
-
-const sendResetEmail = async (email, token, isStaff = false) => {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",        
-    auth: {
-      user: "readoraofficial@gmail.com", 
-      pass: "judjvfwhvacdxdcu",          
-    },
-  });
-
-  // DIFFERENT RESET LINK for staff and user
-  const resetUrl = isStaff 
-    ? `http://localhost:3000/staff/reset-password?token=${token}`
-    : `http://localhost:3000/user/reset-password?token=${token}`;
-
-  const mailOptions = {
-    from: "readora",
-    to: email,
-    subject: "Readora Password Reset Request",
-    text: `You requested a password reset. Use the link below to reset your password:
-    ${resetUrl}
-    This link will expire in 10 minutes.
-    `
-  };
-
-  await transporter.sendMail(mailOptions);
-  console.log("Reset email sent successfully!");
-};
-
+const nodemailer = require('nodemailer');
 
 const sendVerificationEmail = async (email, token) => {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: "readoraofficial@gmail.com",
-      pass: "hjyqvspkssmpwryk",
-    },
-  });
-  const mailOptions = {
-    from: "readora",
-    to: email,
-    subject: "Readora Email Verification",
-    html: `
-      <p>Welcome to Readora!</p>
-      <p>Please verify your email address by clicking the link below:</p>
-      <a href="http://localhost:4000/user/verify?token=${token}">Verify your account</a>
-      <p>If you did not sign up, you can ignore this email.</p>
-    `
-  };
-  await transporter.sendMail(mailOptions);
-  console.log("Verification email sent successfully!");
+  try {
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "readoraofficial@gmail.com",
+        pass: "hjyqvspkssmpwryk",
+      },
+    });
+    const mailOptions = {
+      from: "readora",
+      to: email,
+      subject: "Readora Email Verification",
+      html: `
+        <p>Welcome to Readora!</p>
+        <p>Please verify your email address by clicking the link below:</p>
+        <a href="https://readora.onrender.com/user/verify?token=${token}">Verify your account</a>
+        <p>If you did not sign up, you can ignore this email.</p>
+      `
+    };
+    await transporter.sendMail(mailOptions);
+    console.log("Verification email sent successfully!");
+  } catch (error) {
+    console.error("Error sending verification email:", error);
+  }
 };
 
 module.exports = { sendResetEmail, sendVerificationEmail };
